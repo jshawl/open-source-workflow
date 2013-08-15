eb1877eb2c83a1ab13fa55baa84efb151ac8b2d1
Jesse Shawl <jesse@jshawl.com>
2013-08-15 15:43:53 -0400

    sass compiles on manual `$ grunt`

--- css/Gruntfile.js
+++ css/Gruntfile.js
@@ -1,5 +1,23 @@
 module.exports = function(grunt) {
-  grunt.registerTask('default', 'just a basic hello world', function() {
-    grunt.log.writeln('Grunt is installed and working!');
+  //initialize our Grunt configuration object
+  grunt.initConfig({
+  	//define a task named 'sass'
+  	sass: {
+  	  //output options
+      dist: {
+      	//compressed output
+        options:{
+          style:'compressed'
+        },
+        //compile style.scss to style.css
+        files: {
+          'css/style.css' : 'scss/style.scss'
+        }
+      }
+    }
   });
+  //load our plugin
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  //register a default task
+  grunt.registerTask('default', ['sass']);
 };
