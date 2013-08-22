5a89a941fa07e7facf5fc8bc069235a7e086638b
Jesse Shawl <jesse@jshawl.com>
2013-08-21 21:15:16 -0400

    Update Gruntfile.js

--- css/Gruntfile.js
+++ css/Gruntfile.js
@@ -1,35 +1,40 @@
 module.exports = function(grunt) {
-  //initialize our Grunt configuration object
   grunt.initConfig({
-  	//the 'sass' task
-  	sass: {
-  	  //output options
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
       dist: {
-      	//compressed output
         options:{
-          style:'compressed'
+          style:'compressed' 
         },
-        //compile style.scss to style.css
         files: {
           'css/style.css' : 'scss/style.scss'
         }
       }
     },
-    // the 'watch' task
+    autoprefixer:{
+      dist:{
+        files:{
+          'css/style.css':'css/style.css'
+        }
+      }
+    },
+    csso: {
+      dist: {
+        files: {
+          'css/style.css': ['css/style.css']
+        }
+      }
+    },
     watch: {
-      //watch the scss directory
-      scss: {
-      	//watch any scss file
+      css: {
         files: 'scss/*.scss',
-        //on any change, run the 'sass' task
-        tasks: ['sass']
+        tasks: ['sass','autoprefixer','csso'] 
       }
     }
   });
-  //load sass plugin
   grunt.loadNpmTasks('grunt-contrib-sass');
-  //load watch plugin
   grunt.loadNpmTasks('grunt-contrib-watch');
-  //make the default task 'watch', which watches and compiles Scss files
-  grunt.registerTask('default', ['watch']);
-};
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-csso');
+  grunt.registerTask('default',['watch']);
+}
